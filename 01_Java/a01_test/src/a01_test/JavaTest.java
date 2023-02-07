9e3cf349ef045f2de8c7e8ebfb038c1fcf2188be
lys5034 <lys5034@nate.com>
2023-02-07 17:16:14 +0900

    20230207_변수

---
+++ 01_Java/a01_test/src/a01_test/JavaTest.java
@@ -0,0 +1,34 @@
+package a01_test;
+// 컴파일러 : 코드 짠 거 컴터언어(2진수)로 번역해주는 애
+
+// 주석 : 설명을 작성하는 부분 (컴파일러(<코드 읽는 애)가 해석 X)
+// 한 줄 주석 : 한 줄 전체가 주석 처리
+
+/* 범위 주석 */
+
+//1
+//2
+
+/*
+3
+4
+*/
+
+// class : 자바 코드가 작성되는 영역
+
+public class JavaTest {
+	
+	// main method (메인 메서드)
+	// 자바 프로그램(애플리케이션)을 실행하기 위해 반드시 필요한 구문
+	public static void main(String args[]) {
+		// System.out.println() 
+		// 콘솔에 한 줄 출력하라는 명령어
+        // ln : line (한 줄로 출력)
+		
+		System.out.println("Hello World~!!!!^0^다섯시반..");
+		System.out.println(10+20+30+40+50*2);
+		
+		// ctrl + f11 : 코드 실행(Run)
+	}
+	
+}
