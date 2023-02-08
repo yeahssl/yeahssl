a8a7a040b92a9ab1a88fb581af87531fa11e6129
lys5034 <lys5034@nate.com>
2023-02-08 11:51:30 +0900

    20230208_연산자

---
+++ 01_Java/a002_operator/src/edu/kh/operator/ex/OperatorEx1.java
@@ -0,0 +1,15 @@
+package edu.kh.operator.ex;
+
+import java.util.scanner; // Scanner import 코드
+
+public class OperatorEx1 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in); // Scanner 객체 생성
+		
+		
+		
+	}
+	
+}
