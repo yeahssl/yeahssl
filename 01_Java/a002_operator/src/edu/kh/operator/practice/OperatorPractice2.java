5b8dede16acda06f54d8d1ad076998d17bdffc5b
lys5034 <lys5034@nate.com>
2023-02-13 17:27:46 +0900

    20230213_수정

--- 01_Java/a002_operator/src/edu/kh/operator/practice/OperatorPractice2.java
+++ 01_Java/a002_operator/src/edu/kh/operator/practice/OperatorPractice2.java
@@ -45,27 +45,27 @@ public class OperatorPractice2 {
 		
 	    // System.out.print("번호(정수) : " );
 	    // int input4 = sc.nextInt();
-	    System.out.printf("이름 : " );
-	    String input1 = sc.next();
-	    
-	    System.out.printf("학년(정수) : " );
-	    int input2 = sc.nextInt();
-		
-	    System.out.printf("반(정수) : " );
-	    int input3 = sc.nextInt();		
-		
-	    System.out.printf("번호(정수) : " );
-	    int input4 = sc.nextInt();
-	    
-	    System.out.printf("성별(남학생/여학생) : " );
-	    String input5 = sc.next();
-	    
-	    System.out.printf("성적(소수점 아래 둘째 자리까지) : " );
-	  
-	    double input6 = sc.nextDouble();
-	    
-	    System.out.printf("%d학년 %d반 %d번 %s %s의 성적은 %.2f점 입니다." 
-	    ,input2, input3, input4, input1, input5, input6); 
+		// System.out.printf("이름 : " );
+		// String input1 = sc.next();
+		//	    
+		// System.out.printf("학년(정수) : " );
+		// int input2 = sc.nextInt();
+		//		
+		// System.out.printf("반(정수) : " );
+		// int input3 = sc.nextInt();		
+		//		
+		// System.out.printf("번호(정수) : " );
+		// int input4 = sc.nextInt();
+		//	    
+		// System.out.printf("성별(남학생/여학생) : " );
+		// String input5 = sc.next();
+		//	    
+		// System.out.printf("성적(소수점 아래 둘째 자리까지) : " );
+		//	  
+		// double input6 = sc.nextDouble();
+		//	    
+		// System.out.printf("%d학년 %d반 %d번 %s %s의 성적은 %.2f점 입니다." 
+		// ,input2, input3, input4, input1, input5, input6); 
 	   
 	    // System.out.print("성별(남학생/여학생) : " );
 	    // String input5 = sc.next();
