f036c66986d794627022687cddda6a916406cb1d
lys5034 <lys5034@nate.com>
2023-02-14 17:33:41 +0900

    20230214_배열 연습예제 및  객체지향 언어

---
+++ 01_Java/a004_array/src/edu/kh/array/practice/ArrayPracticeRun.java
@@ -0,0 +1,16 @@
+package edu.kh.array.practice;
+
+public class ArrayPracticeRun {
+	public static void main(String[] args) {
+		
+		ArrayPractice ap = new ArrayPractice(); {
+			
+			
+			ap.ex1();
+			
+		}
+		
+		
+	}
+
+}
