d94ccae54476061634627e2c2344e87829d69bf1
lys5034 <lys5034@nate.com>
2023-02-23 16:34:24 +0900

    20230223_ 추상클래스2 및 예외처리

---
+++ 01_Java/a009_exception/src/edu/kh/exception/run/ExceptionRun.java
@@ -0,0 +1,16 @@
+package edu.kh.exception.run;
+
+import java.io.IOException;
+
+import edu.kh.exception.service.ExceptionService;
+
+public class ExceptionRun {
+	
+	public static void main(String[] args) throws IOException {
+		
+		ExceptionService service = new ExceptionService();
+		
+		service.ex3();
+	}
+
+}
