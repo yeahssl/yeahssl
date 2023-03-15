1d7e2cacf4d594813b053d02e42a4bb9b5e4a11b
lys5034 <lys5034@nate.com>
2023-03-15 19:48:35 +0900

    no message

--- 02_DB/workbook/02_FUNCTION.sql
+++ 02_DB/workbook/02_FUNCTION.sql
@@ -14,9 +14,10 @@ WHERE PROFESSOR_NAME NOT LIKE '___';
 -- 3. 춘 기술대학교의 남자 교수들의 이름과 나이를 출력하는 SQL 문장을 작성하시오. 단 이때 나이가 적은 
 --    사람에서 많은 사람 순서로 화면에 출력되도록 만드시오. (단, 교수 중 2000 년 이후 출생자는 없으며
 --    출력 해더는 "교수이름, 나이"로 한다. 나이는 '만'으로 계산한다.)
-SELECT PROFESSOR_NAME, 
-FLOOR(MONTHS_BETWEEN(SYSDATE(TO_DATE((SUBSTR(PROFESSOR_SSN,1,6))),'RRMMDD') / 12))
+SELECT PROFESSOR_NAME 교수이름,
+FLOOR(MONTHS_BETWEEN(SYSDATE,TO_DATE(19||SUBSTR(PROFESSOR_SSN,1,6),'RR/MM/DD')) / 12) 나이
 FROM TB_PROFESSOR
+WHERE SUBSTR(PROFESSOR_SSN,8,1)='1' 
 ORDER BY PROFESSOR_SSN ASC; 
 
 -- 4. 교수들의 이름 중 성을 제외한 이름만 출력하는 SQL 문장을 작성하시오. 출력 헤더는 "이름"이 
