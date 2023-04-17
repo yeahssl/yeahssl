955f4784966c4f26b9008fa87844815a6a63ce1d
DESKTOP-IJ8C807\user1 <lys5034@nate.com>
2023-04-17 18:43:57 +0900

    20230417_스터디 외계어사전 문제 수정(코드리뷰)

--- 01_Java/team.prectice/src/team/prectice/first/Solution3.java
+++ 01_Java/team.prectice/src/team/prectice/first/Solution3.java
@@ -2,32 +2,27 @@ package team.prectice.first;
 
 public class Solution3 {
 	public static void main(String args[]) {
-		String[] spell = {"p", "o", "s"};
-		String[] dic = {"sod", "eocd", "qixm", "adio", "soo"};
-		
+		String[] spell = { "s", "o", "d" };
+		String[] dic = { "sod", "eocd", "qixm", "adio", "soo" };
+
 		System.out.println(solution(spell, dic));
 	}
-	
-	    public static int solution(String[] spell, String[] dic) {
-	        int answer = 0;
-	       
-	        
-	        for(int i = 0; i < dic.length; i++) {
-	        	int count = 1;
-	        	for(int j = 0; j < spell.length; j++) {
-	        		if(spell[j].contains(dic[i])) {
-	        			count++;
-	        		
-	        		} if(count == spell.length) {
-	        			answer = 1;
-	        			return answer;
-	        		}else {
-	        			answer = 2;
-	        		}
-	        	}
-	        }
-	        return answer;
-	    }
+
+	public static int solution(String[] spell, String[] dic) {
+		int answer = 2;
+
+		for (int i = 0; i < dic.length; i++) {
+			int count = 0;
+			
+			for (int j = 0; j < spell.length; j++) {
+				if (dic[i].contains(spell[j])) {
+					count++;
+				} 
+			}
+			if (count == spell.length) {
+				answer = 1;
+			}
+		}
+		return answer;
 	}
-
-
+}
