cdbccf50ab5e838745f55e4bd1770a3d4d8e4097
yeahssl <lys5034@nate.com>
2023-06-12 10:39:19 +0900

    20230612_스프링부트 에러페이지

---
+++ 07.SpringBoot/boardProject-Boot/src/main/java/edu/kh/project/board/model/dao/BoardMapper.java
@@ -0,0 +1,18 @@
+package edu.kh.project.board.model.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface BoardMapper {
+
+	/** 게시판 종류 
+	 * @return
+	 */
+	public List<Map<String, Object>> selectBoardTypeList();
+	
+	
+
+}
