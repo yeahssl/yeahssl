9e07f7af255f3ba77347270fc88eccfe7624e61e
yeahssl <lys5034@nate.com>
2023-07-06 09:37:11 +0900

    리엑트 수업 todo List

--- 08_react/my-app/src/App.js
+++ 08_react/my-app/src/App.js
@@ -11,6 +11,10 @@ import State1 from './components/R02_state1';
 import State2 from './components/R03_state2';
 import State3 from './components/R04_state3';
 
+import TodoList1 from './components/R05_todolist1';
+import ContextApi from './components/R06_context_api';
+
+
 function App() {
   // 리액트의 컴포넌트는 딱 하나의 요소만을 반환할 수 있다
   // -> 여러 요소를 반환하고 싶을때는 부모 요소로 묶어준다!
@@ -19,8 +23,8 @@ function App() {
     /* fragment(<> </>) : 반환되는 요소를 감쌀 때 사용, 해석 x */
     <>
       {/* jsx 주석 */}
-      <h1>Hello React!!</h1>
-      <div>와 리액트다</div>
+      {/* <h1>Hello React!!</h1> */}
+      {/* <div>와 리액트다</div> */}
       {/* <Ex1/> */}
       {/* <Ex2/> */}
 
@@ -28,14 +32,19 @@ function App() {
       {/* <PropsEx name={'김길동'}/> */}
 
       {/* R02_state1 */}
-      <State1 />
+      {/* <State1 /> */}
       {/* R03_state2 */}
-      <State2 init={100}/>
+      {/* <State2 init={100}/> */}
 
       {/* R04_state3 */}
-      <hr/>
-      <State3 />
+      {/* <hr/> */}
+      {/* <State3 /> */}
 
+      {/* R05_todoList1 */}
+      {/* <TodoList1 /> */}
+
+      {/* R06_context_api */}
+      <ContextApi />
     </>
   );
 }
